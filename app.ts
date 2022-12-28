e7b9d37d411aa88245e45a87cdfca91c82e6f895
theDcreative <tangdesign.sci@gmail.com>
2022-12-28 15:54:25 +0700

    lab1.2 test

--- app.ts
+++ app.ts
@@ -1,9 +1,29 @@
-let text: string;
-text = 'Hello World';
+// let text: string;
+// text = 'Hello World';
 
-let textName: string;
-textName = 'Chatpong';
+// let textName: string;
+// textName = 'Chatpong';
 
 
-console.log(text);
-console.log(textName);
+// console.log(text);
+// console.log(textName);
+
+//funtion length
+// 1.
+let text1: string = 'Chatpong';
+console.log('Length of text1 :' + text1.length);
+
+//2.
+for(var val of text1) {
+    console.log(val);
+}
+
+//3.
+for(var val1 of text1){
+    console.log(`indexOf ${val1} is ` + text1.indexOf(val1));
+}
+
+//5.
+let txt1: string = 'Hello';
+let txt2: string = 'World';
+console.log(txt1 + ' '+ txt2);
--- out/app.js
+++ out/app.js
@@ -1,7 +1,25 @@
-var text;
-text = 'Hello World';
-var textName;
-textName = 'Chatpong';
-console.log(text);
-console.log(textName);
+// let text: string;
+// text = 'Hello World';
+// let textName: string;
+// textName = 'Chatpong';
+// console.log(text);
+// console.log(textName);
+//funtion length
+// 1.
+var text1 = 'Chatpong';
+console.log('Length of text1 :' + text1.length);
+//2.
+for (var _i = 0, text1_1 = text1; _i < text1_1.length; _i++) {
+    var val = text1_1[_i];
+    console.log(val);
+}
+//3.
+for (var _a = 0, text1_2 = text1; _a < text1_2.length; _a++) {
+    var val1 = text1_2[_a];
+    console.log("indexOf ".concat(val1, " is ") + text1.indexOf(val1));
+}
+//5.
+var txt1 = 'Hello';
+var txt2 = 'World';
+console.log(txt1 + ' ' + txt2);
 //# sourceMappingURL=app.js.map
