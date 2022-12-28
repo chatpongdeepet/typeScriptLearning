85c93a54025caad13b1b8329d77b9431f612684a
theDcreative <tangdesign.sci@gmail.com>
2022-12-28 21:48:57 +0700

    template tring part 1

--- app.ts
+++ app.ts
@@ -9,21 +9,36 @@
 // console.log(textName);
 
 //funtion length
-// 1.
-let text1: string = 'Chatpong';
-console.log('Length of text1 :' + text1.length);
+// // 1.
+// let text1: string = 'Chatpong';
+// console.log('Length of text1 :' + text1.length);
 
-//2.
-for(var val of text1) {
-    console.log(val);
-}
+// //2.
+// for(var val of text1) {
+//     console.log(val);
+// }
 
-//3.
-for(var val1 of text1){
-    console.log(`indexOf ${val1} is ` + text1.indexOf(val1));
-}
+// //3.
+// for(var val1 of text1){
+//     console.log(`indexOf ${val1} is ` + text1.indexOf(val1));
+// }
 
-//5.
-let txt1: string = 'Hello';
-let txt2: string = 'World';
-console.log(txt1 + ' '+ txt2);
+// //5.
+// let txt1: string = 'Hello';
+// let txt2: string = 'World';
+// console.log(txt1 + ' '+ txt2);
+
+let txt1 : string = 'microsoft excel';
+console.log(txt1.toUpperCase());
+
+let txt2 : string = 'GOOGLE AND APPLE';
+console.log(txt2.toLowerCase());
+
+let txt3 : string = 'hello world';
+var splitted = txt3.split(" ");
+var helloTxt = splitted[0];
+var HelloTxt = helloTxt.charAt(0).toUpperCase() + helloTxt.slice(1);
+var worldTxt = splitted[1];
+var WorldTxt = worldTxt.charAt(0).toUpperCase() + worldTxt.slice(1);
+
+console.log(HelloTxt + " " + WorldTxt);
--- out/app.js
+++ out/app.js
@@ -5,21 +5,30 @@
 // console.log(text);
 // console.log(textName);
 //funtion length
-// 1.
-var text1 = 'Chatpong';
-console.log('Length of text1 :' + text1.length);
-//2.
-for (var _i = 0, text1_1 = text1; _i < text1_1.length; _i++) {
-    var val = text1_1[_i];
-    console.log(val);
-}
-//3.
-for (var _a = 0, text1_2 = text1; _a < text1_2.length; _a++) {
-    var val1 = text1_2[_a];
-    console.log("indexOf ".concat(val1, " is ") + text1.indexOf(val1));
-}
-//5.
-var txt1 = 'Hello';
-var txt2 = 'World';
-console.log(txt1 + ' ' + txt2);
+// // 1.
+// let text1: string = 'Chatpong';
+// console.log('Length of text1 :' + text1.length);
+// //2.
+// for(var val of text1) {
+//     console.log(val);
+// }
+// //3.
+// for(var val1 of text1){
+//     console.log(`indexOf ${val1} is ` + text1.indexOf(val1));
+// }
+// //5.
+// let txt1: string = 'Hello';
+// let txt2: string = 'World';
+// console.log(txt1 + ' '+ txt2);
+var txt1 = 'microsoft excel';
+console.log(txt1.toUpperCase());
+var txt2 = 'GOOGLE AND APPLE';
+console.log(txt2.toLowerCase());
+var txt3 = 'hello world';
+var splitted = txt3.split(" ");
+var helloTxt = splitted[0];
+var HelloTxt = helloTxt.charAt(0).toUpperCase() + helloTxt.slice(1);
+var worldTxt = splitted[1];
+var WorldTxt = worldTxt.charAt(0).toUpperCase() + worldTxt.slice(1);
+console.log(HelloTxt + " " + WorldTxt);
 //# sourceMappingURL=app.js.map
