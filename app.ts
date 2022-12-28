88c84d5995f9084234bcbccbfbadfab74da5ff38
theDcreative <tangdesign.sci@gmail.com>
2022-12-28 15:18:49 +0700

    first commit

---
+++ app.ts
@@ -0,0 +1,9 @@
+let text: string;
+text = 'Hello World';
+
+let textName: string;
+textName = 'Chatpong';
+
+
+console.log(text);
+console.log(textName);
---
+++ out/app.js
@@ -0,0 +1,7 @@
+var text;
+text = 'Hello World';
+var textName;
+textName = 'Chatpong';
+console.log(text);
+console.log(textName);
+//# sourceMappingURL=app.js.map
